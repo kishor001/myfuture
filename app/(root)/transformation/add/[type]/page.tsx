98634780c27a0909256d225e2af4a737b8748b05
kishor001 <kishoracharya99@gmail.com>
2024-05-04 17:00:27 +0545

    initial commit

---
+++ app/(root)/transformation/add/[type]/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from '@/components/shared/Header'
+import { transformationTypes } from '@/constants'
+import TransformationForm from '@/components/shared/TransformationForm';
+
+const AddTransformationTypePage = ({ params: {type}}: SearchParamProps) => {
+  const transformation = transformationTypes[type];
+
+  return (
+    <>
+    <Header 
+    title= {transformation.title}
+    subtitle={transformation.subTitle}
+    />
+    <TransformationForm/>
+    </>
+  )
+}
+
+export default AddTransformationTypePage
