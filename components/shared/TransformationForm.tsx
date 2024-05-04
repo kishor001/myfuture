32544ec01fc949261f1dbe890e3990afb57d589f
kishor001 <kishoracharya99@gmail.com>
2024-05-04 17:44:17 +0545

    Update TransformationForm.tsx

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -18,19 +18,19 @@ const formSchema = z.object({
     username: z.string().min(2).max(50),
   })
 // 1. Define your form
-const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      username: "",
-    },
-  })
+// const form = useForm<z.infer<typeof formSchema>>({
+//     resolver: zodResolver(formSchema),
+//     defaultValues: {
+//       username: "",
+//     },
+//   })
  
-  // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
-  }
+//   // 2. Define a submit handler.
+//   function onSubmit(values: z.infer<typeof formSchema>) {
+//     // Do something with the form values.
+//     // ✅ This will be type-safe and validated.
+//     console.log(values)
+//   }
 
 
 
@@ -39,28 +39,29 @@ import React from 'react'
 
 const TransformationForm = () => {
   return (
+    <div>Transformation</div>
 
-        <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-             )}
-             />
-             <Button type="submit">Submit</Button>
-           </form>
-         </Form>
+        // <Form {...form}>
+        // <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        // <FormField
+        //   control={form.control}
+        //   name="username"
+        //   render={({ field }) => (
+        //     <FormItem>
+        //       <FormLabel>Username</FormLabel>
+        //       <FormControl>
+        //         <Input placeholder="shadcn" {...field} />
+        //       </FormControl>
+        //       <FormDescription>
+        //         This is your public display name.
+        //       </FormDescription>
+        //       <FormMessage />
+        //     </FormItem>
+        //      )}
+        //      />
+        //      <Button type="submit">Submit</Button>
+        //    </form>
+        //  </Form>
 
   )
 }
